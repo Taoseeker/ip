47a5db69469134e67f95567e8de359aafd167a25
Taoseeker <e1155335@u.nus.edu>
2025-01-24 00:19:37 +0800

    Added a todo list

--- src/main/java/NeoChat.java
+++ src/main/java/NeoChat.java
@@ -1,9 +1,12 @@
 import java.util.*;
 
 public class NeoChat {
+    private static ArrayList<String> toDo = new ArrayList<>(100);
+    private static int index = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String BYE = "bye";
+        String LIST = "list";
         System.out.println("Hello! I am NeoChat!");
 
         System.out.println("What can I do for you?" + "\n");
@@ -12,10 +15,22 @@ public class NeoChat {
             if (BYE.equals(userInput)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (LIST.equals(userInput)) {
+                showList();
+            } else {
+                toDo.add(userInput);
+                index++;
+                System.out.println("added: " + userInput);
             }
-            System.out.println(userInput);
         }
-
-
+    }
+    private static void showList() {
+            if(index == 0) {
+                System.out.println("Empty todo list!");
+            } else {
+                for (int i = 0; i < index; i++) {
+                    System.out.println("" + (i + 1)  + ". " + toDo.get(i));
+                }
+            }
     }
 }
