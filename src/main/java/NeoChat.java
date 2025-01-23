c1890acf9a7e4e0c53c3291323cf479fe9d6b296
Taoseeker <e1155335@u.nus.edu>
2025-01-23 11:49:26 +0800

    Renamed chatbot and added a skeletal structure

---
+++ src/main/java/NeoChat.java
@@ -0,0 +1,10 @@
+public class NeoChat {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I am NeoChat!");
+
+        System.out.println("What can I do for you?" + "\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
