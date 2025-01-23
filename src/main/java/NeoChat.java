cfb2a1448d81ef32aa0b20f45abe799e6e6737cb
Taoseeker <e1155335@u.nus.edu>
2025-01-24 00:03:26 +0800

    Added echo feature

--- src/main/java/NeoChat.java
+++ src/main/java/NeoChat.java
@@ -1,10 +1,21 @@
+import java.util.*;
+
 public class NeoChat {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String BYE = "bye";
         System.out.println("Hello! I am NeoChat!");
 
         System.out.println("What can I do for you?" + "\n");
+        while (true) {
+            String userInput = sc.nextLine();
+            if (BYE.equals(userInput)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(userInput);
+        }
 
-        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
